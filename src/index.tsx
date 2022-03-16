90ee9909ef2961f5750b74325f3a2168d31e3f06
이찬호 <0407chan@naver.com>
2022-03-16 09:30:40 +0900

    feat. index에 AppContainer반영

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import AppContainer from './containers/AppContainer'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
   document.getElementById('root')
 )
