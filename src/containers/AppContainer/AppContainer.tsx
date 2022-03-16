6c6f9db6af61ee85714858ff04262dba9dab3e17
이찬호 <0407chan@naver.com>
2022-03-16 09:26:50 +0900

    feat. create AppContainer

---
+++ src/containers/AppContainer/AppContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Woowahan Link
+          <code>Welcome</code>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/AppContainer/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const hello = styled.div``
+
+export const hello2 = styled.div``
