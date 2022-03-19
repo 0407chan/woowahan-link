c914c96e30d381be25edc8ee9517ac5d3031a724
이찬호 <0407chan@naver.com>
2022-03-19 20:34:39 +0900

    feat. 헤어꾸미디, gridblock적용

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import GridBlock from '../../components/GridBlock'
+import LinkBlock from '../../components/LinkBlock'
+import { UrlData } from '../../constants/data'
 import * as S from './style'
 
 const AppContainer: React.FC = () => {
   return (
     <S.Container>
-      <header className="App-header">
-        <p>
-          Woowahan Link
-          <code>Welcome</code>
-        </p>
-      </header>
+      <S.Header>
+        <GridBlock grid={1} style={{ paddingLeft: 20 }}>
+          <S.StyledText>우아한 링크</S.StyledText>
+        </GridBlock>
+        <GridBlock grid={2} style={{ justifyContent: 'center' }}>
+          <input />
+        </GridBlock>
+        <GridBlock
+          grid={1}
+          style={{ paddingRight: 20, justifyContent: 'flex-end' }}
+        >
+          <button type="button">호호</button>
+        </GridBlock>
+      </S.Header>
+      <S.Body>
+        {UrlData.map((url) => (
+          <LinkBlock key={url.id} link={url} />
+        ))}
+      </S.Body>
     </S.Container>
   )
 }
--- src/containers/AppContainer/style.ts
+++ src/containers/AppContainer/style.ts
@@ -1,13 +1,41 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Header = styled.div`
   display: flex;
-  justify-content: center;
+  width: 100%;
+  max-width: 1320px;
+  justify-content: space-between;
   align-items: center;
-  background-color: #282c34;
-  color: #ffffff;
-  height: 100vh;
-  font-size: 44px;
+  height: 60px;
+`
+export const Body = styled.div`
+  display: flex;
+  width: calc(100% - 40px);
+  height: fit-content;
+  max-width: calc(1320px - 40px);
+  padding: 20px;
+  gap: 40px;
+  flex-direction: row;
+  flex-wrap: wrap;
 `
 
-export const hello2 = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background: linear-gradient(104.97deg, #fef9ff 0%, #f2f6ff 100%);
+  height: 100vh;
+`
+
+export const StyledText = styled.span`
+  position: relative;
+  z-index: 1;
+  font-weight: 700;
+  font-size: 30px;
+  background: linear-gradient(95.03deg, #45a6ff 0%, #f03eff 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+`
