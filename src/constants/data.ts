4207e0289c1327dfd8f709ebd4cdba140d64b713
이찬호 <0407chan@naver.com>
2022-03-21 23:24:50 +0900

    더미데이터 추가

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -28,5 +28,17 @@ export const UrlData: LinkType[] = [
     url: 'https://iframely.com/embed/https%3A%2F%2Fwww.figma.com%2Ffile%2F0pQwl6VAVeLXZibSNRQtN2%2F%25EC%259A%25B0%25EC%2595%2584%25ED%2595%259C-%25EB%25A7%2581%25ED%2581%25AC%3Fnode-id%3D0%253A1?_scaling=scale-down',
     title: '테스트',
     service: '테스트'
+  },
+  {
+    id: uuid(),
+    url: 'https://iframely.com/embed/https%3A%2F%2Fwww.figma.com%2Ffile%2F0pQwl6VAVeLXZibSNRQtN2%2F%25EC%259A%25B0%25EC%2595%2584%25ED%2595%259C-%25EB%25A7%2581%25ED%2581%25AC%3Fnode-id%3D0%253A1?_scaling=scale-down',
+    title: '테스트',
+    service: '테스트'
+  },
+  {
+    id: uuid(),
+    url: 'https://iframely.com/embed/https%3A%2F%2Fwww.figma.com%2Ffile%2F0pQwl6VAVeLXZibSNRQtN2%2F%25EC%259A%25B0%25EC%2595%2584%25ED%2595%259C-%25EB%25A7%2581%25ED%2581%25AC%3Fnode-id%3D0%253A1?_scaling=scale-down',
+    title: '테스트',
+    service: '테스트'
   }
 ]
