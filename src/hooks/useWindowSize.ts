38f4147f41ca8fe2e8a24827c2826fa4fd827d56
이찬호 <0407chan@naver.com>
2022-03-23 11:35:15 +0900

    windowSize hook추가

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+type WindowSize = {
+  width?: number
+  height?: number
+}
+
+const useWindowSize = (): {
+  windowSize: WindowSize
+  isMobile: () => boolean
+} => {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: undefined,
+    height: undefined
+  })
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const isMobile = () => {
+    if (windowSize.width === undefined) return false
+    return windowSize.width <= 990
+  }
+
+  return {
+    windowSize,
+    isMobile
+  }
+}
+
+export default useWindowSize
