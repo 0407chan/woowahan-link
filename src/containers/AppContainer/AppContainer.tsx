c3862d6afdeba10b40763f5b87da66e8fcee6158
이찬호 <0407chan@naver.com>
2022-04-13 08:59:19 +0900

    app에서 header컴포넌트 제거

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -1,53 +1,32 @@
 import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import GridBlock from '../../components/GridBlock'
+import Header from '../../components/Header'
 import LinkBlock from '../../components/LinkBlock'
 import { LinkType, UrlData } from '../../constants/data'
-import { IMAGES } from '../../constants/image'
 import { darkTheme, lightTheme } from '../../constants/themes'
 import { useDarkMode } from '../../hooks/useDarkMode'
-import useWindowSize from '../../hooks/useWindowSize'
 import * as S from './style'
 
 const AppContainer: React.FC = () => {
-  const { isMobile } = useWindowSize()
   const { theme, themeToggler } = useDarkMode()
 
   const [searchKey, setSearchKey] = useState<string>('')
   const [linkList] = useState<LinkType[]>(UrlData)
 
-  const onAddNewLink = () => {
-    window.open(
-      'https://forms.gle/HHcTcwjtTyhtnDvy9',
-      '_blank'
-    )
-  }
-  const onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearch = (newSearchKey:string) => {
     setSearchKey(
-      event.target.value || ''
+      newSearchKey
     )
   }
+
   return (
     <ThemeProvider theme={theme === 'LIGHT' ? lightTheme : darkTheme}>
       <S.Container>
-        <S.Header>
-          <GridBlock grid={1}>
-            <S.StyledImage alt="logo" src={IMAGES.logo} />
-            <S.StyledText>우아한 링크</S.StyledText>
-          </GridBlock>
-          <GridBlock grid={2} style={{ justifyContent: 'center' }}>
-            <S.SearchInput
-              type="text"
-              value={searchKey}
-              onChange={onSearch}
-              placeholder="검색어를 입력해주세요"
-            />
-          </GridBlock>
-          <GridBlock grid={1} style={{ justifyContent: 'flex-end' }}>
-            <S.AddButton onClick={themeToggler}>{isMobile() ? <img alt="add-round" src={IMAGES.addRound} /> : ('다크')}</S.AddButton>
-            <S.AddButton onClick={onAddNewLink}>{isMobile() ? <img alt="add-round" src={IMAGES.addRound} /> : ('새 링크 추가')}</S.AddButton>
-          </GridBlock>
-        </S.Header>
+        <Header
+          searchKey={searchKey}
+          onSearch={handleSearch}
+          themeToggler={themeToggler}
+        />
         <S.Body>
           <S.LinkContainer>
             {linkList
--- src/containers/AppContainer/style.ts
+++ src/containers/AppContainer/style.ts
@@ -1,21 +1,5 @@
 import styled from 'styled-components'
 
-export const Header = styled.div`
-  @media (max-width: 990px) {
-    gap: 20px;
-  }
-
-  display: flex;
-  width: calc(100% - 40px);
-  max-width: calc(1320px - 40px);
-  padding: 0 20px;
-  gap: 40px;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
-  min-height: 60px;
-  max-height: 60px;
-`
 export const Body = styled.div`
   display: flex;
   width: 100%;
@@ -59,49 +43,6 @@ export const Container = styled.div`
   /* background: linear-gradient(104.97deg, #fef9ff 0%, #f2f6ff 100%); */
   background-color: ${({ theme }) => theme.background};
   height: 100vh;
-`
-
-export const StyledText = styled.span`
-  @media (max-width: 990px) {
-    display: none;
-  }
-  position: relative;
-  z-index: 1;
-  margin-left:10px;
-  font-weight: 700;
-  font-size: 30px;
-  background: linear-gradient(95.03deg, #45a6ff 0%, #f03eff 100%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-  text-fill-color: transparent;
-`
-export const StyledImage = styled.img`
-  width: 38px;
-  height: 38px;
-`
-
-export const SearchInput = styled.input`
-  border: unset;
-  max-width: 620px;
-  height: 36px;
-  width: 100%;
-  background-color: ${({ theme }) => theme.blockBackground};
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  padding-left: 20px;
-  font-size: 16px;
-  transition: box-shadow 0.2s ease;
-  &:focus {
-    outline: #eeeeee solid 1px;
-    box-shadow: 0 0 16px 2px #e1d5ff !important;
-  }
-  &:hover {
-    box-shadow: 0 0 8px 2px #e1d5ff;
-  }
-  &::placeholder {
-    color: #d1d1d1;
-  }
 `
 
 export const AddButton = styled.button`
