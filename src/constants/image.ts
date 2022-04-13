220bcc3e5a16601f3abe9e60bcdf819d2abe392c
이찬호 <0407chan@naver.com>
2022-04-13 19:52:03 +0900

    feat. Image 추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,5 +1,7 @@
 export const IMAGES = {
   logo: `${process.env.PUBLIC_URL}/logo512.png`,
   addRound: `${process.env.PUBLIC_URL}/images/Add_round.png`,
-  copy: `${process.env.PUBLIC_URL}/images/Copy.png`
+  copy: `${process.env.PUBLIC_URL}/images/Copy.png`,
+  MOON_FILL: `${process.env.PUBLIC_URL}/images/Moon_fill.png`,
+  SUN_FILL: `${process.env.PUBLIC_URL}/images/Sun_fill.png`
 }
