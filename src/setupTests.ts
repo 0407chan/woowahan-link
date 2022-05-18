18928bc718bdd12d16dbb3440e22ef76874e02f7
이찬호 <0407chan@naver.com>
2022-05-18 20:52:13 +0900

    jest 세팅

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import 'given2/setup';
+import 'jest-plugin-context/setup';
+
