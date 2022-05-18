51d85fa45343becb33b0276973772ed7f809ad98
이찬호 <0407chan@naver.com>
2022-05-18 20:53:58 +0900

    링크 블럭 복사 제거

--- src/components/LinkBlock/LinkBlock.tsx
+++ src/components/LinkBlock/LinkBlock.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-array-index-key */
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { LinkType } from '../../constants/data'
-import { IMAGES } from '../../constants/image'
+import Horizontal from '../Horizontal'
 import * as S from './style'
 
 type Props = {
@@ -11,7 +11,6 @@ type Props = {
 
 const LinkBlock: React.FC<Props> = ({ link, searchKey }) => {
   const copyRef = useRef<HTMLInputElement>(null)
-  const [isClipCopied, setIsClipCopied] = useState<boolean>(false)
 
   const highlightDiv = (value?: string) => {
     if (!searchKey) return value
@@ -31,20 +30,6 @@ const LinkBlock: React.FC<Props> = ({ link, searchKey }) => {
     )
   }
 
-  const onUrlClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    event.stopPropagation()
-    if (copyRef.current) {
-      console.log(isClipCopied)
-      copyRef.current.focus()
-      copyRef.current.select()
-      document.execCommand('copy')
-      setIsClipCopied(true)
-      setTimeout(() => {
-        setIsClipCopied(false)
-      }, 800)
-    }
-  }
-
   return (
     <S.Container
       onClick={() => {
@@ -56,7 +41,14 @@ const LinkBlock: React.FC<Props> = ({ link, searchKey }) => {
         return null
       }}
     >
-      <S.Title>{highlightDiv(link.title)}</S.Title>
+      <Horizontal style={{ marginBottom: 10 }}>
+        <S.Title>{highlightDiv(link.title)}</S.Title>
+        <S.Name>
+          -
+          {' '}
+          {link.name}
+        </S.Name>
+      </Horizontal>
 
       <input
         type="text"
@@ -67,29 +59,14 @@ const LinkBlock: React.FC<Props> = ({ link, searchKey }) => {
           position: 'relative', top: 5, left: 5, zIndex: -1, height: 1, width: 1
         }}
       />
-      {/* <S.InfoText
-        className={isClipCopied ? 'show' : ''}
-        style={{ marginTop: 0 }}
-      >
-        <CheckCircleFilled style={{ color: '#52c41a' }} />
-        클립보드에 복사됨
-      </S.InfoText> */}
-      <S.UrlContainer onClick={onUrlClick}>
-        <S.Url>
-          {highlightDiv(link.url)}
-        </S.Url>
-        <S.CopyButton type="button">
-          <img alt="copy-button" draggable={false} src={IMAGES.copy} />
-        </S.CopyButton>
-      </S.UrlContainer>
 
       {(link.tags?.length ?? 0) > 0 && (
         <S.TagContainer>
           {link.tags?.map((item, index) => (
-            <>
-              <div key={item}>{highlightDiv(item)}</div>
+            <React.Fragment key={item}>
+              <div>{highlightDiv(item)}</div>
               {(link.tags?.length ?? 0) - 1 !== index && <div>·</div>}
-            </>
+            </React.Fragment>
           ))}
         </S.TagContainer>
       )}
