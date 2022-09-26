f4ae530e4b8bf4e7e35b793ee719b35245a18807
이찬호 <0407chan@naver.com>
2022-09-26 11:57:23 +0900

    feat. 리액트 쿼리 반영

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import AppContainer from './containers/AppContainer'
 import './index.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      keepPreviousData: true,
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <AppContainer />
+    <QueryClientProvider client={queryClient}>
+      <AppContainer />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
