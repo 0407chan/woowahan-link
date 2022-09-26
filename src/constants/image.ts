1710ba92e9b36791dd894301b2e9e2da1d417fa2
이찬호 <0407chan@naver.com>
2022-09-26 11:56:34 +0900

    feat. image constants에 이미지 파일 추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -3,5 +3,9 @@ export const IMAGES = {
   addRound: `${process.env.PUBLIC_URL}/images/Add_round.png`,
   copy: `${process.env.PUBLIC_URL}/images/Copy.png`,
   MOON_FILL: `${process.env.PUBLIC_URL}/images/Moon_fill.png`,
-  SUN_FILL: `${process.env.PUBLIC_URL}/images/Sun_fill.png`
+  SUN_FILL: `${process.env.PUBLIC_URL}/images/Sun_fill.png`,
+
+  empty_state: `${process.env.PUBLIC_URL}/images/empty_state.png`,
+  empty_tung: `${process.env.PUBLIC_URL}/images/empty_tung.png`,
+  error_her: `${process.env.PUBLIC_URL}/images/error_her.png`,
 }
