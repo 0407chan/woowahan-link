14f4b5c3002bae63a954ebdc5ae711000892fe79
이찬호 <0407chan@naver.com>
2022-09-28 00:41:58 +0900

    chore. WLModal props정리

--- src/components/WLModal/WLModal.tsx
+++ src/components/WLModal/WLModal.tsx
@@ -1,17 +1,11 @@
 import { ModalProps } from 'antd/lib/modal'
 import * as S from './style'
 
-export type WLModalProps = {} & ModalProps
+export type WLModalProps = ModalProps
 
 const WLModal: React.FC<WLModalProps> = ({ children, ...props }) => {
   return (
-    <S.StyledModal
-      open
-      transitionName=""
-      okText="등록"
-      cancelText="취소"
-      {...props}
-    >
+    <S.StyledModal open transitionName="" {...props}>
       {children}
     </S.StyledModal>
   )
