a9c4e41a461cf7ede45cc4e9a8cad776719f7890
이찬호 <0407chan@naver.com>
2022-10-01 16:27:35 +0900

    config. error.png추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -2,10 +2,14 @@ export const IMAGES = {
   logo: `${process.env.PUBLIC_URL}/logo512.png`,
   addRound: `${process.env.PUBLIC_URL}/images/Add_round.png`,
   copy: `${process.env.PUBLIC_URL}/images/Copy.png`,
+
+  // DarkMode
   MOON_FILL: `${process.env.PUBLIC_URL}/images/Moon_fill.png`,
   SUN_FILL: `${process.env.PUBLIC_URL}/images/Sun_fill.png`,
 
+  // Error
   empty_state: `${process.env.PUBLIC_URL}/images/empty_state.png`,
   empty_tung: `${process.env.PUBLIC_URL}/images/empty_tung.png`,
   error_her: `${process.env.PUBLIC_URL}/images/error_her.png`,
+  error: `${process.env.PUBLIC_URL}/images/error.png`
 }
