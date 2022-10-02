d0b1ae87ae1de9f4d38fe26fd2a23bd75e4672cd
이찬호 <chanho@woowahan.com>
2022-10-02 18:56:41 +0900

    config. icon image들 추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -11,5 +11,24 @@ export const IMAGES = {
   empty_state: `${process.env.PUBLIC_URL}/images/empty_state.png`,
   empty_tung: `${process.env.PUBLIC_URL}/images/empty_tung.png`,
   error_her: `${process.env.PUBLIC_URL}/images/error_her.png`,
-  error: `${process.env.PUBLIC_URL}/images/error.png`
+  error: `${process.env.PUBLIC_URL}/images/error.png`,
+
+  ICON: {
+    COPY: {
+      DARK: `${process.env.PUBLIC_URL}/images/icon/icon-dark-copy.png`,
+      LIGHT: `${process.env.PUBLIC_URL}/images/icon/icon-light-copy.png`
+    },
+    LINK: {
+      DARK: `${process.env.PUBLIC_URL}/images/icon/icon-dark-link.png`,
+      LIGHT: `${process.env.PUBLIC_URL}/images/icon/icon-light-link.png`
+    },
+    REMOVE: {
+      DARK: `${process.env.PUBLIC_URL}/images/icon/icon-dark-remove.png`,
+      LIGHT: `${process.env.PUBLIC_URL}/images/icon/icon-light-remove.png`
+    },
+    UPDATE: {
+      DARK: `${process.env.PUBLIC_URL}/images/icon/icon-dark-update.png`,
+      LIGHT: `${process.env.PUBLIC_URL}/images/icon/icon-light-update.png`
+    }
+  }
 }
