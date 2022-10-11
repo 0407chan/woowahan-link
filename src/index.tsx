25034c435a9f0c5513a3d364990d9ca32b40d73d
이찬호 <0407chan@naver.com>
2022-10-12 00:53:23 +0900

    [10] redux provider추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Provider } from 'react-redux'
 import AppContainer from './containers/AppContainer'
 import { GlobalStyle } from './globals.styles'
 import './index.less'
+import { store } from './store'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,10 +20,12 @@ const queryClient = new QueryClient({
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <AppContainer />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <AppContainer />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
